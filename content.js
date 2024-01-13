a597c997f34e20cda68fef7b8b482b23af310cd5
Frank Smith <fasvi6@gmail.com>
2024-01-13 11:45:16 -0500

    finally figured out pikmin drawing quirk; Don't set canvas css height and width!

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
 const C = document.createElement('canvas')
+C.width=document.body.clientWidth
+C.height=document.body.clientHeight
+const CTX = C.getContext('2d')
 
 const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
     const { top, left, bottom, right } = el.getBoundingClientRect();
@@ -12,13 +15,10 @@ const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
 
 function addPikminToCanvas() {
     const pikmin = new Image()
-    pikmin.onload = () => { ctx.drawImage(pikmin, 10, 10) }
-    pikmin.src = chrome.runtime.getURL('./images/red-pikmin.webp')
-    pikmin.style.height = "20px"
-    pikmin.style.width = "auto"
-    console.log(`pikmin height:${pikmin.style.height}`)
-    const ctx = C.getContext('2d')
-    ctx.drawImage(pikmin, 100, 100)
+    pikmin.onload = () => {
+        CTX.drawImage(pikmin, 0, 0, 20, 25)
+    }
+    pikmin.src = chrome.runtime.getURL('./images/red-pikmin.png')
 }
 
 function attachOnion() {
@@ -40,8 +40,6 @@ function attachCanvas() {
     C.style.zIndex = "9999"
     C.style.pointerEvents = "none"
     C.style.position = "absolute"
-    C.style.height = "100%"
-    C.style.width = "100%"
     C.style.inset = "0"
 
     document.body.append(C)
