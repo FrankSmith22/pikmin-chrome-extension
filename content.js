954fe7674d9e9d604c55030da34927a3ad8f4fcd
Frank Smith <fasvi6@gmail.com>
2024-01-13 17:08:40 -0500

    bobbing animation!

--- content.js
+++ content.js
@@ -9,13 +9,53 @@ const ONION_HEIGHT = 150
 const PIKMIN_ARMY = []
 
 class Sprite {
-    constructor({ position, image }){
+    constructor({ position, image, width, height }){
         this.position = position
         this.image = image
+        this.width = width
+        this.height = height
     }
 
     draw() {
-        CTX.drawImage(this.image, this.position.x, this.position.y, 20, 25)
+        CTX.drawImage(this.image, this.position.x, this.position.y, this.width, this.height) // 20 and 25
+    }
+}
+
+class PikminSprite extends Sprite {
+    constructor({ position, image, width, height }){
+        super({ position, image, width, height })
+        this.bobStart = position.y // Make sure this is set as pikmin moves around the screen
+        this.bobIndex = null
+        
+        // Looks crazy, but this is a list of the pixels away from bobStart to animate while bobbing
+        this.BOB_ANIMATION = [1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1]
+    }
+    
+    draw() {
+        // First, if pikmin is already bobbing
+        if(this.bobIndex !== null){
+            if(this.bobIndex === this.BOB_ANIMATION.length - 1){
+                // We've reached the end of the bob animation
+                this.bobIndex = null
+                this.position.y = this.bobStart
+            }
+            else {
+                this.bobIndex++
+                this.position.y = this.bobStart + this.BOB_ANIMATION[this.bobIndex]
+            }
+        }
+        else {
+            // Randomly decide if pikmin should animate. Make it 1/5 chance
+            const shouldBob = Math.floor(Math.random() * 100) <= 1 //(True == 0 or 1)
+            console.log(`should bob: ${shouldBob}`)
+            if(shouldBob){
+                // Begin bobbing animation
+                this.isBobbing = true
+                this.bobIndex = 0
+                this.position.y = this.bobStart + this.BOB_ANIMATION[this.bobIndex]
+            }
+        }
+        super.draw()
     }
 }
 
@@ -32,12 +72,14 @@ const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
 function addPikminToCanvas() {    
     const pikminImage = new Image()
     pikminImage.onload = () => {
-        const pikmin = new Sprite({
+        const pikmin = new PikminSprite({
             position:{
                 x: Math.floor(Math.random() * ONION_WIDTH),
                 y: Math.floor(Math.random() * ONION_HEIGHT)
             },
-            image: pikminImage
+            image: pikminImage,
+            width: 20,
+            height: 25
         })
         PIKMIN_ARMY.push(pikmin)
     }
@@ -73,7 +115,7 @@ function attachCanvas() {
 function animate(){
     // Grab ref to animation ID, might use later
     const animationId = window.requestAnimationFrame(animate)
-    console.log("Hello! Running loop")
+    CTX.clearRect(0, 0, C.width, C.height);
     for(pikmin of PIKMIN_ARMY){
         pikmin.draw()
     }
