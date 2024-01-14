953b42a185b51ff384e102384079bf8071318910
Frank Smith <fasvi6@gmail.com>
2024-01-14 11:43:09 -0500

    Rerender all (fully) visible images on page onto canvas as Target sprites

---
+++ classes.js
@@ -0,0 +1,62 @@
+
+class Sprite {
+    constructor({ position, image, width, height }){
+        this.position = position
+        this.image = image
+        this.width = width
+        this.height = height
+    }
+    
+    draw() {
+        CTX.drawImage(this.image, this.position.x, this.position.y, this.width, this.height) // 20 and 25
+    }
+}
+
+class Target extends Sprite{
+    constructor({ left, top, right, bottom, x, y, width, height, image, alive=true }){
+        super({position: {x, y}, image, width, height})
+        this.left = left
+        this.top = top
+        this.right = right
+        this.bottom = bottom
+        this.alive = alive
+    }
+}
+
+class PikminSprite extends Sprite {
+    constructor({ position, image, width, height }){
+        super({ position, image, width, height })
+        this.bobStart = position.y // Make sure this is set as pikmin moves around the screen
+        this.bobIndex = null
+        
+        // Looks crazy, but this is a list of the pixels away from bobStart to animate while bobbing
+        this.BOB_ANIMATION = [1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1]
+    }
+    
+    draw() {
+        // First, if pikmin is already bobbing
+        if(this.bobIndex !== null){
+            if(this.bobIndex === this.BOB_ANIMATION.length - 1){
+                // We've reached the end of the bob animation
+                this.bobIndex = null
+                this.position.y = this.bobStart
+            }
+            else {
+                this.bobIndex++
+                this.position.y = this.bobStart - this.BOB_ANIMATION[this.bobIndex]
+            }
+        }
+        else {
+            // Randomly decide if pikmin should animate. Make it 2% chance
+            const shouldBob = Math.floor(Math.random() * 100) <= 1 //(True == 0 or 1)
+            console.log(`should bob: ${shouldBob}`)
+            if(shouldBob){
+                // Begin bobbing animation
+                this.isBobbing = true
+                this.bobIndex = 0
+                this.position.y = this.bobStart - this.BOB_ANIMATION[this.bobIndex]
+            }
+        }
+        super.draw()
+    }
+}
--- content.js
+++ content.js
@@ -7,57 +7,7 @@ const ONION_WIDTH = 180
 const ONION_HEIGHT = 150
 
 const PIKMIN_ARMY = []
-
-class Sprite {
-    constructor({ position, image, width, height }){
-        this.position = position
-        this.image = image
-        this.width = width
-        this.height = height
-    }
-
-    draw() {
-        CTX.drawImage(this.image, this.position.x, this.position.y, this.width, this.height) // 20 and 25
-    }
-}
-
-class PikminSprite extends Sprite {
-    constructor({ position, image, width, height }){
-        super({ position, image, width, height })
-        this.bobStart = position.y // Make sure this is set as pikmin moves around the screen
-        this.bobIndex = null
-        
-        // Looks crazy, but this is a list of the pixels away from bobStart to animate while bobbing
-        this.BOB_ANIMATION = [1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1]
-    }
-    
-    draw() {
-        // First, if pikmin is already bobbing
-        if(this.bobIndex !== null){
-            if(this.bobIndex === this.BOB_ANIMATION.length - 1){
-                // We've reached the end of the bob animation
-                this.bobIndex = null
-                this.position.y = this.bobStart
-            }
-            else {
-                this.bobIndex++
-                this.position.y = this.bobStart - this.BOB_ANIMATION[this.bobIndex]
-            }
-        }
-        else {
-            // Randomly decide if pikmin should animate. Make it 1/5 chance
-            const shouldBob = Math.floor(Math.random() * 100) <= 1 //(True == 0 or 1)
-            console.log(`should bob: ${shouldBob}`)
-            if(shouldBob){
-                // Begin bobbing animation
-                this.isBobbing = true
-                this.bobIndex = 0
-                this.position.y = this.bobStart - this.BOB_ANIMATION[this.bobIndex]
-            }
-        }
-        super.draw()
-    }
-}
+let TARGETS = []
 
 const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
     const { top, left, bottom, right } = el.getBoundingClientRect();
@@ -86,6 +36,15 @@ function addPikminToCanvas() {
     pikminImage.src = chrome.runtime.getURL('./images/red-pikmin.png')
 }
 
+function attachCanvas() {
+    C.style.zIndex = "9999"
+    C.style.pointerEvents = "none"
+    C.style.position = "absolute"
+    C.style.inset = "0"
+
+    document.body.append(C)
+}
+
 function attachOnion() {
     const onion = document.createElement('img')
     onion.src = chrome.runtime.getURL('./images/onion.png')
@@ -95,38 +54,36 @@ function attachOnion() {
     onion.style.left = "0"
     onion.style.height = `${ONION_HEIGHT}px`
     onion.style.width = `${ONION_WIDTH}px`
-
+    
     onion.addEventListener('click', addPikminToCanvas)
     
     document.body.prepend(onion)
-}
-
-function attachCanvas() {
-    C.style.zIndex = "9999"
-    C.style.pointerEvents = "none"
-    C.style.position = "absolute"
-    C.style.inset = "0"
-
-    document.body.append(C)
-
-    animate()
 }
 
 function animate(){
     // Grab ref to animation ID, might use later
     const animationId = window.requestAnimationFrame(animate)
     CTX.clearRect(0, 0, C.width, C.height);
-    for(pikmin of PIKMIN_ARMY){
+    for(let pikmin of PIKMIN_ARMY){
         pikmin.draw()
+    }
+    for(let target of TARGETS){
+        target.draw()
     }
 }
 
 function run() {
     const allImages = document.querySelectorAll("img")
     const visibleImages = [...allImages].filter(image => elementIsVisibleInViewport(image))
-    console.log(visibleImages)
+    TARGETS = visibleImages.map(image => {
+        const imageRect = image.getBoundingClientRect().toJSON()
+        imageRect.x += 100
+        imageRect.y += 100
+        return new Target({...imageRect, image })
+    })
     attachCanvas()
     attachOnion()
+    animate()
 }
 
 run()
